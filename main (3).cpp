b2405075c145532493f36e75c24a9b6a97ef831d
Elisa Medina <147352316+KittiePower@users.noreply.github.com>
2023-10-08 16:52:39 -0700

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+Ciclo For
+Nombre: Elisa Michelle Medina Vázquez 
+Fecha: 090522
+*******************************************************************************/
+
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+int i ;
+ 
+for(i=1; i<=100; i++  ){
+ 
+cout<< i <<endl;
+}
+ 
+cin.get();
+return 0;
+}
